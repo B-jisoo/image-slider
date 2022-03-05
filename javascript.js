f86024fdb4de53b139bb6834a783ed7abfa1c5fe
B-jisoo <mrk334kr@naver.com>
2022-03-05 19:53:48 +0900

    slide3
    js의 classList를 이용한 자동 슬라이드 구현

--- javascript.js
+++ javascript.js
@@ -26,3 +26,26 @@ slideNext.addEventListener("click", function () {
     currentIdx = 1;
   }
 });
+
+////////////////////////////////////////////////////
+
+const SHOWING_CLASS = "showing";
+const firstSlide = document.querySelector(".swiper-slide:first-child");
+
+function slide() {
+  const currentSlide = document.querySelector(`.${SHOWING_CLASS}`);
+
+  if (currentSlide) {
+    currentSlide.classList.remove(SHOWING_CLASS);
+    const nextSlide = currentSlide.nextElementSibling;
+    if (nextSlide) {
+      nextSlide.classList.add(SHOWING_CLASS);
+    } else {
+      firstSlide.classList.add(SHOWING_CLASS);
+    }
+  } else {
+    firstSlide.classList.add(SHOWING_CLASS);
+  }
+}
+slide();
+setInterval(slide, 2000);
